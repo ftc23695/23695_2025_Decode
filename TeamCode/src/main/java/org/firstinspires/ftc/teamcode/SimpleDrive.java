10881f4d825dc23ee577c744563f84585d0b953d
ftc23695 <ftc23695@gmail.com>
2025-11-03 19:37:23 -0700

    Simple Drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+    @TeleOp
+    public class SimpleDrive extends OpMode
+    {
+        private DcMotor leftFrontDrive = null;
+        private DcMotor leftBackDrive = null;
+        private DcMotor rightFrontDrive = null;
+        private DcMotor rightBackDrive = null;
+
+        @Override
+        public void init() {
+            //Declare variables for phone to recognise//
+
+            //names on the config
+
+            leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front");
+            rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front");
+            leftBackDrive = hardwareMap.get(DcMotor.class, "left_back");
+            rightBackDrive = hardwareMap.get(DcMotor.class, "right_back");
+
+
+            leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+            rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+            leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+            rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+            leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+            telemetry.addData("status", "Initialized");
+        }
+
+
+        //Set variables//
+        @Override
+        public void loop() {
+            double leftFrontPower;
+            double rightFrontPower;
+            double leftBackPower;
+            double rightBackPower;
+
+
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+            leftFrontPower = Range.clip(drive + turn + strafe, -1, 1);
+            rightFrontPower = Range.clip(drive - turn - strafe, -1, 1);
+            leftBackPower = Range.clip(drive + turn - strafe, -1, 1);
+            rightBackPower = Range.clip(drive - turn + strafe, -1, 1);
+
+            if(gamepad1.right_bumper){
+                leftFrontPower /= 2;
+                leftBackPower /= 2;
+                rightFrontPower /= 2;
+                rightBackPower /= 2;
+            }
+
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightBackDrive.setPower(rightBackPower);
+
+            //Claw Code: Opens with GP2 X and opens less when past vertical position
+            // BIGGER CLOSES MORE*********************
+
+        }
+
+        @Override
+        public void stop() {
+            leftFrontDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            leftBackDrive.setPower(0);
+            rightBackDrive.setPower(0);
+
+
+        }
+
+    }
+}
