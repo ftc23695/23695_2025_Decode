00f49321451d1fcb417a4c285554888b93679daf
ftc23695 <ftc23695@gmail.com>
2026-01-15 19:23:04 -0700

    changed variable, just getting updated repository

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTesting.java
@@ -137,7 +137,7 @@ public class AprilTagTesting extends OpMode
         // joystick controls
         double drive = -gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
-        double turn = turningPower;
+        double turn = gamepad1.right_stick_x;
         // making sure power doesnt exceed 100% and adding and subtracting variables to
         // get individual motor power levels
         leftFrontPower = Range.clip(drive + turn + strafe, -1, 1);
